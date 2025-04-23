908f25ff47dcfaec41b06c058533c0f6befbaad3
김경환 <k2sc2000@gmail.com>
2025-04-23 14:48:10 +0900

    feat: image upload logiz 구현 S12P31S306-99

---
+++ backend/src/main/java/com/lumina/backend/post/service/S3Service.java
@@ -0,0 +1,21 @@
+package com.lumina.backend.post.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * 사진 관련 비즈니스 로직을 처리하는 서비스 인터페이스
+ */
+public interface S3Service {
+
+    /**
+     * 프로필 이미지를 S3에 업로드하는 메서드
+     */
+    String uploadImageFile(MultipartFile file, String folderName) throws IOException;
+
+    /**
+     * S3 파일 URL 생성
+     */
+    String getFileUrl(String folder, String fileName);
+}
