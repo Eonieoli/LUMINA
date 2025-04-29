750ccc35925f877ff93e404c5e36139270fa277a
홍석진 <seokbangguri@kakao.com>
2025-04-29 09:10:57 +0900

    feat: 게시물 컴포넌트
    S12P31S306-167

---
+++ frontend/src/pages/main/Home.tsx
@@ -0,0 +1,67 @@
+import { Board } from "@/components/boards/Board"
+
+const posts = [
+    {
+        "postId": 11,
+        "userId":3,
+        "nickname":"박우배",
+        "profileImage": "http://dummyimage.com/112x100.png/dddddd/000000",
+        "postImage": "http://dummyimage.com/112x100.png/dddddd/000000",
+        "postContent": "다같이 선행해요",
+        "postViews": 3,
+        "categoryName": "사람",
+        "hashtagList": ["행복", "사랑"],
+        "likeCnt": 4,
+        "commentCnt":15,
+        "isLike": true
+    },
+    {
+        "postId": 1,
+        "userId":2,
+        "nickname":"박우배",
+        "profileImage": "http://dummyimage.com/112x100.png/dddddd/000000",
+        "postImage": "http://dummyimage.com/112x100.png/dddddd/000000",
+        "postContent": "다같이 선행해요",
+        "postViews": 3,
+        "categoryName": "사람",
+        "hashtagList": ["행복", "사랑"],
+        "likeCnt": 4,
+        "commentCnt":15,
+        "isLike": true
+    },
+    {
+        "postId": 2,
+        "userId":1,
+        "nickname":"박우배",
+        "profileImage": "http://dummyimage.com/112x100.png/dddddd/000000",
+        "postImage": "http://dummyimage.com/225x100.png/dddddd/000000",
+        "postContent": "다같이 선행해요",
+        "postViews": 3,
+        "categoryName": "사람",
+        "hashtagList": ["행복", "사랑"],
+        "likeCnt": 4,
+        "commentCnt":15,
+        "isLike": true
+    },
+]
+
+export const Home = () => {
+    return (
+        <div>
+            {posts.map((post) => (
+                <Board
+                    key={post.postId}
+                    postId={post.postId}
+                    nickname={post.nickname}
+                    profileImage={post.profileImage}
+                    postImage={post.postImage}
+                    categoryName={post.categoryName}
+                    postContent={post.postContent}
+                    likeCnt={post.likeCnt}
+                    commentCnt={post.commentCnt}
+                    isLike={post.isLike}
+                />
+            ))}
+        </div>
+    );
+};
