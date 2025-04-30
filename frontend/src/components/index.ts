7a5fe2f5fe62514c684594df54c64d8f1ec198e8
홍석진 <seokbangguri@kakao.com>
2025-04-30 16:24:42 +0900

    feat: 댓글 바텀시트
    S12P31S306-174

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,10 +4,14 @@ import Header from "./Header";
 // 게시판 관련
 import { Board } from "./boards/Board";
 import { Comments } from "./boards/Comments";
+import { BottomSheet } from "./boards/BottomSheet";
+import { Replies } from "./boards/Replies";
 
 export {
     Navbar,
     Header,
     Board,
     Comments,
+    BottomSheet,
+    Replies,
 }
