751d781bf7ebce5500db608bbce5681ef9de15cc
김민정 <99minj0731@naver.com>
2025-05-01 00:56:00 +0900

    fix: 검색창 폰트크기 수정 및 키보드이벤트 추가가

--- frontend/src/components/donate/SearchBar.tsx
+++ frontend/src/components/donate/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { SearchIconGray } from "@/assets/images"
+import React from "react"
 
 interface DonationSearchProps {
   keyword: string
@@ -8,20 +9,29 @@ interface DonationSearchProps {
 }
 
 export default function DonateSearchBar({keyword, setKeyword, onSearchClick, autoFocus = false}: DonationSearchProps) {
+
+  // 엔터를 쳐도 검색이 가능하도록
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === "Enter") {
+      onSearchClick()
+    }
+  }
+
   return (
-    <div className="flex items-center w-full h-7.5 border border-gray-300 rounded-3xl">
+    <div className="flex items-center w-full h-9 border border-gray-300 rounded-3xl">
       <input 
         type="text"
         placeholder="기부처를 검색해보세요."
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}  
-        className="flex-1 outline-none text-sm ml-4"
+        className="flex-1 outline-none text-[16px] ml-4"
         autoFocus={autoFocus}
+        onKeyDown={handleKeyDown}
       />
       <img 
         src={SearchIconGray} 
         alt="searchIcon" 
-        className="w-3 mr-4 text-gray-300" 
+        className="w-3 m-4 text-gray-300" 
         onClick={onSearchClick}
       />
     </div>
