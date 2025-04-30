55fd99c765656e6b5305fa55cba56d2036b75536
홍석진 <seokbangguri@kakao.com>
2025-04-30 16:28:37 +0900

    style: navbar z-index 조절 및 main에 relative 추가

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -28,7 +28,7 @@ export default function Main() {
     <>
       {/* 옆에 네비게이션 있으니까 왼쪽과 하단에 패딩값을 넣음 */}
       <div className="flex justify-center pb-20 md:pb-0 md:pl-20"> 
-        <div className="w-full md:w-[468px]">
+        <div className="relative h-dvh overflow-y-scroll w-full md:w-[468px]">
           <Routes>
             {/* 경로가 정확히 부모 경로와 일치할 때 홈페이지를 보여줌 */}
             <Route index element={<HomePage/>} />
