6f49d0cc94288e98980c7478608306175698eb56
홍석진 <seokbangguri@kakao.com>
2025-05-02 15:16:11 +0900

    style: navbar 스타일 조정

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -8,7 +8,7 @@ export default function Main() {
     <>
       {/* 옆에 네비게이션 있으니까 왼쪽과 하단에 패딩값을 넣음 */}
       <div className="flex justify-center pb-20 md:pb-0 md:pl-20"> 
-        <div id="scrollable-container" className="h-dvh overflow-y-scroll w-full md:w-[468px]">
+        <div id="scrollable-container" className="relative h-dvh overflow-y-scroll w-full min-w-80 md:w-[468px]">
           <Routes>
             {/* 경로가 정확히 부모 경로와 일치할 때 홈페이지를 보여줌 */}
             <Route index element={<HomePage/>} />
@@ -22,8 +22,8 @@ export default function Main() {
             <Route path="donate/:donationId" element={<DonationDetailPage/>} />
           </Routes>
         </div>
-      </div>
       <Navbar/>
+      </div>
     </>
   )
 }
