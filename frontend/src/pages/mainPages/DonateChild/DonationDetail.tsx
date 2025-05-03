1ed91ce712b52df5a6ceb139dfd2a266f32fa741
김민정 <99minj0731@naver.com>
2025-05-03 11:51:52 +0900

    style: 기부금액입력 페이지 레이아웃 S12P31S306-228

--- frontend/src/pages/mainPages/DonateChild/DonationDetail.tsx
+++ frontend/src/pages/mainPages/DonateChild/DonationDetail.tsx
@@ -71,7 +71,7 @@ export default function DonationDetailPage() {
   if(!donation) return <div>존재하지 않는 기부처!</div>
 
   return (
-    <div className="w-full h-full px-6 py-6">
+    <div className="w-full h-full px-6 py-6 mb-20">
 
       {/* 상단바 */}
       <div className="flex h-6 items-center justify-between mb-4">
@@ -79,61 +79,64 @@ export default function DonationDetailPage() {
         <PointInfo />
       </div>
 
-      {/* 기부처 상단 정보 */}
-      <div className="flex flex-col justify-center items-center mb-6">
+      <div>
+        {/* 기부처 상단 정보 */}
+        <div className="flex flex-col justify-center items-center mb-6">
 
-        <div className="h-[150px] w-[260px] overflow-hidden flex items-center justify-center mb-2">
-          <img src={donationImg} alt="" className="max-w-full max-h-full object-contain"/>
-        </div>
-
-        <div className="mb-2 flex items-center">
-          <p className="text-xl font-bold text-gray-600 mr-2">{donation.donationName}</p>
-          <div>
-            {donation.isSubscribe ? 
-              <img src={HeartFilledIcon} alt="구독중" onClick={handleToggleSubscribe} />
-              :
-              <img src={HeartDefaultIcon} alt="구독안함" onClick={handleToggleSubscribe}/>
-            }
+          <div className="h-[150px] w-[260px] overflow-hidden flex items-center justify-center mb-2">
+            <img src={donationImg} alt="" className="max-w-full max-h-full object-contain"/>
           </div>
-        </div>
-        <p className="text-[16px] text-gray-600 text-center w-[260px]">생명에 대한 존중과 차별없는 동물권을 지향하는 시민들의 모임</p>
 
+          <div className="mb-2 flex items-center">
+            <p className="text-xl font-bold text-gray-600 mr-2">{donation.donationName}</p>
+            <div>
+              {donation.isSubscribe ? 
+                <img src={HeartFilledIcon} alt="구독중" onClick={handleToggleSubscribe} />
+                :
+                <img src={HeartDefaultIcon} alt="구독안함" onClick={handleToggleSubscribe}/>
+              }
+            </div>
+          </div>
+          <p className="text-[16px] text-gray-600 text-center w-[260px]">생명에 대한 존중과 차별없는 동물권을 지향하는 시민들의 모임</p>
+
+        </div>
+
+        {/* 기부처 하단 정보 */}
+        <div className="w-full border-gray-200 rounded-2xl p-5 border-2 text-gray-600 font-medium text-[17px]">
+          
+          <div className="flex justify-between">
+            <p className="font-semibold">총 기부 금액 </p>
+            <div className="flex">
+              <p className="text-[#5D56F1]">{donation.sumPoint}</p>
+              <p>&nbsp;/&nbsp;100</p>
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <p className="font-semibold">누적 기부자 수</p>
+            <p>{donation.sumUser}명</p>
+          </div>
+
+          <div className="flex justify-between mt-10">
+            <p className="font-semibold">나의 기부 횟수</p>
+            <p>{donation.myDonationCnt}회</p>
+          </div>
+
+          <div className="flex justify-between">
+            <p className="font-semibold">나의 기부 누적금액</p>
+            <div className="flex items-center">
+              <p className="mr-1">{donation.mySumDonation}</p>
+              <img src={CoinIcon} alt="포인트이미지" className="w-4 object-contain "/>
+            </div>
+          </div>
+
+        </div>
       </div>
 
-      {/* 기부처 하단 정보 */}
-      <div className="w-full border-gray-200 rounded-2xl p-5 border-2 text-gray-600 font-medium text-[17px]">
-        
-        <div className="flex justify-between">
-          <p className="font-semibold">총 기부 금액 </p>
-          <div className="flex">
-            <p className="text-[#5D56F1]">{donation.sumPoint}</p>
-            <p>&nbsp;/&nbsp;100</p>
-          </div>
-        </div>
-
-        <div className="flex justify-between">
-          <p className="font-semibold">누적 기부자 수</p>
-          <p>{donation.sumUser}명</p>
-        </div>
-
-        <div className="flex justify-between mt-10">
-          <p className="font-semibold">나의 기부 횟수</p>
-          <p>{donation.myDonationCnt}회</p>
-        </div>
-
-        <div className="flex justify-between">
-          <p className="font-semibold">나의 기부 누적금액</p>
-          <div className="flex items-center">
-            <p className="mr-1">{donation.mySumDonation}</p>
-            <img src={CoinIcon} alt="포인트이미지" className="w-4 object-contain "/>
-          </div>
-        </div>
-
-      </div>
 
       {/* 기부하기 버튼*/}
       <button 
-        className="bg-[#9C97FA] w-full p-3 text-white rounded-2xl text-[20px] font-normal cursor-pointer mt-5 md:mt-10 hover:bg-[#5D56F1] transition-colors duration-300 "
+        className="bg-[#9C97FA] w-full p-3 text-white rounded-2xl text-[20px] font-normal cursor-pointer mt-5 md: md:mt-15 hover:bg-[#5D56F1] transition-colors duration-300 "
         onClick={goToDonate}        
         >
         기부하기
--- frontend/src/pages/mainPages/DonateChild/DonationInput.tsx
+++ frontend/src/pages/mainPages/DonateChild/DonationInput.tsx
@@ -1,7 +1,65 @@
+import { useNavigate } from "react-router-dom"
+import { BackIcon, CoinIcon, WinkLuna } from "@/assets/images"
+import PointInfo from "@/components/donate/PointInfo"
+import { useState } from "react"
+
 export default function DonationInputPage() {
+
+  const navigate = useNavigate()
+  // const {donationId} = useParams()
+
+  const [point, setPoint] = useState<string>("") // HTML의 input 요소는 항상 문자열로 값을 반환하기 때문에
+
+  // 뒤로가기 버튼 누르기
+  const goToBack = () => {
+    navigate(-1)
+  }
+
   return (
-    <div>
-      기부 금액 입력하는 페이지!!!
+    <div className="w-full h-full px-6 py-6 mb-20">
+
+      {/* 상단바 */}
+      <div className="flex h-6 items-center justify-between mb-4">
+        <img src={BackIcon} alt="BackIcon" className="w-5" onClick={goToBack}/>
+        <PointInfo/>
+      </div>
+
+      <div className="flex flex-col justify-center items-center" >
+
+        {/* 안내 멘트 */}
+        <div className="text-[20px] font-medium text-gray-600 mt-20">
+          <p>
+            기부하실
+            <span className="font-bold text-[#5D56F1]">포인트</span>
+            <span className="font-bold">를 입력</span>
+            해주세요!
+          </p>
+        </div>
+
+        {/* 루미나 이미지 */}
+          <img src={WinkLuna} alt="윙크하는루미나" className="w-60 mt-15 mb-15"/>  
+        
+        {/* 포인트 입력창 */}
+        <div className="flex justify-center items-center w-2/3 pb-0.5 text-gray-400">
+          <input 
+            type="text" 
+            value={point}
+            onChange={(e) => {setPoint(e.target.value)}}
+            placeholder="100"
+            className="text-2xl font-semibold text-center outline-none w-full"
+          />
+          <img src={CoinIcon} alt="코인" className="w-6" />
+        </div>
+
+      </div>
+    
+
+    {/* 확인 버튼 */}
+    <button 
+        className="bg-[#9C97FA] w-full p-3 text-white rounded-2xl text-[20px] font-normal cursor-pointer mt-5 md: md:mt-15 hover:bg-[#5D56F1] transition-colors duration-300 "
+        >
+        확인
+      </button>
     </div>
   )
 }
