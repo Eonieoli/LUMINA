e7b2a880cea45c8ca24a75fbebceefcfccf2fc05
김민정 <99minj0731@naver.com>
2025-05-04 21:55:34 +0900

    feat: 랭킹 조회 api 연결

--- frontend/src/pages/mainPages/Ranking.tsx
+++ frontend/src/pages/mainPages/Ranking.tsx
@@ -1,3 +1,41 @@
-export default function RankingPage() {
-    return <div>랭킹페이지</div>;
+import { RankingInfo } from "@/apis/ranking";
+import { useEffect, useState } from "react";
+
+interface rankingList {
+  userId: number
+  nickname: string
+  profileImage: string
+  sumPoint: number
+  rank: number
 }
+
+export default function RankingPage() {
+
+  const [rankings, setRankings] = useState<rankingList[]>([])
+
+  // const myRanking = rankings[0]
+  const topTen = rankings.slice(1)
+
+  // 랭킹 정보 가져오기
+  useEffect (() => {
+    const fetchRankings = async () => {
+      try {
+        const response = await RankingInfo()
+        setRankings(response)
+      }
+      catch (error) {}
+    }
+    fetchRankings()
+  },[])
+
+
+  return (
+  <div>
+    {topTen.map((user) => (
+      <div key={user.userId}>
+        {user.nickname}
+      </div>
+    ))}
+  </div>
+
+)}
