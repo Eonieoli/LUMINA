c76f62a0a8f6c1d63f499ec85893884c8c797386
홍석진 <seokbangguri@kakao.com>
2025-05-08 14:09:36 +0900

    style: 댓글 애니메이션 적용

--- frontend/src/components/boards/BottomSheet.tsx
+++ frontend/src/components/boards/BottomSheet.tsx
@@ -1,11 +1,13 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { useEffect } from 'react';
 
 interface BottomSheetProps {
     onClose: () => void;
     children: React.ReactNode;
+    isVisible: boolean;
 }
 
-export const BottomSheet = ({ onClose, children }: BottomSheetProps) => {
+export const BottomSheet = ({ onClose, children, isVisible }: BottomSheetProps) => {
     useEffect(() => {
         document.body.style.overflow = 'hidden';
         return () => {
@@ -18,13 +20,25 @@ export const BottomSheet = ({ onClose, children }: BottomSheetProps) => {
             className="bg-opacity-50 fixed inset-0 z-50 flex items-end justify-center bg-[#00000050]"
             onClick={onClose}
         >
-            <div
-                className="max-h-[80%] w-full overflow-y-auto rounded-t-2xl bg-white p-4"
-                onClick={(e) => e.stopPropagation()}
-            >
-                <div className="mx-auto mb-3 h-1.5 w-12 rounded-full bg-gray-300" />
-                {children}
-            </div>
+            <AnimatePresence>
+                {isVisible && (
+                    <motion.div
+                    initial={{ opacity: 0, y: 100 }} // 처음 상태
+                    animate={{ opacity: 1, y: 0 }}  // 등장 시
+                    exit={{ opacity: 0, y: 100 }}    // 사라질 때
+                    transition={{ duration: 0.5 }}
+                    className="your-class"
+                    >
+                        <div
+                            className="max-h-[80%] w-full overflow-y-auto rounded-t-2xl bg-white p-4"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <div className="mx-auto mb-3 h-1.5 w-12 rounded-full bg-gray-300" />
+                            {children}
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
