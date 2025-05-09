b83aff27e8cafa1bc03c3f47e942cf2bb87b3c1e
김민정 <99minj0731@naver.com>
2025-05-09 15:37:51 +0900

    refactor: 마이페이지 게시물 레이아웃 수정

--- frontend/src/pages/mainPages/MyPage.tsx
+++ frontend/src/pages/mainPages/MyPage.tsx
@@ -188,7 +188,7 @@ export default function MyPage() {
             {/* 루미나 선행도 그래프 */}
 
             {/* 유저 게시물 렌더링*/}
-            <div className="bg-white">
+            <div className="bg-white pb-[80px] md:pb-0">
                 {userPosts.length === 0 ? (
                     <div  className="text-center border-t-3 border-gray-300 pt-10 text-gray-500 flex flex-col items-center gap-5 justify-center">
                             <img src={ComputerTypingLuna} alt="루나이미지" className="w-30" />
