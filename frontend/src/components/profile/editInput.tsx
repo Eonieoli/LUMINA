07a7b62440f932c706f7bb8089a8f453669519e4
김민정 <99minj0731@naver.com>
2025-05-10 14:18:58 +0900

    refactor: 프로필 수정 유효성 검사 에러메세지

--- frontend/src/components/profile/editInput.tsx
+++ frontend/src/components/profile/editInput.tsx
@@ -6,15 +6,13 @@ interface EditInputProps {
 
 export default function EditInput ({title, value, onChange}:EditInputProps) {
   return (
-    <div className="w-2/3">
-        <div className="flex flex-col mb-5">
-          <p className="mb-2 ml-2 text-gray-800">{title}</p>
-          <div className="w-full h-9 items-center rounded-[10px] border border-gray-300 flex">
-            <input type="text" value={value} onChange={onChange}
-              className="border-none ml-4 text-gray-600"
-            />
-          </div>
-        </div>
+    <div className="flex flex-col mb-1 mt-5">
+      <p className="mb-2 ml-2 text-gray-800">{title}</p>
+      <div className="w-full h-9 items-center rounded-[10px] border border-gray-300 flex">
+        <input type="text" value={value} onChange={onChange}
+          className="border-none  outline-none w-full ml-4 mr-4 text-gray-600"
+        />
+      </div>
     </div>
   )
 }
--- frontend/src/pages/mainPages/MyPageChild/ProfileEdit.tsx
+++ frontend/src/pages/mainPages/MyPageChild/ProfileEdit.tsx
@@ -18,6 +18,9 @@ export default function ProfileEditPage () {
   const [message, setMessage] = useState<string>("")
   const [profileImageFile, setProfileImageFile] = useState<File | string | null>(null)
 
+  const [nicknameError, setNicknameError] = useState<string>("")
+  const [messageError, setMessageError] = useState<string>("")
+
   // 이미지 선택
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
@@ -39,7 +42,25 @@ export default function ProfileEditPage () {
 
   //프로필 수정 api 부르기
   const handleEditProfile = async () => {
-    console.log("프로필 수정해볼게!!")
+
+    let changeValid = true
+
+    if(nickname.trim() === "") {
+      setNicknameError("닉네임을 입력해주세요.")
+      changeValid = false
+    } else{
+      setNicknameError("")
+    }
+
+    if(message.trim() === "") {
+      setMessageError("상태 메세지를 입력해주세요.")
+      changeValid = false
+    } else{
+      setMessageError("")
+    }
+
+    if (!changeValid) return;
+
     try {
       const formData = new FormData()
       formData.append("nickname", nickname)
@@ -77,20 +98,22 @@ export default function ProfileEditPage () {
                   alt="프로필 이미지" 
                   className="w-40 bg-white rounded-full object-cover aspect-square border-2 border-gray-200"
                 />
-                <img src={PencilIcon} alt="수정아이콘" className=" absolute w-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                <img src={PencilIcon} alt="수정아이콘" className=" absolute w-5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
               </div>
               <input type="file" accept="image/*" onChange={handleImageChange} className="hidden"/>
             </label>
             <img src={CircleXIcon} alt="이미지 삭제" onClick={() => setProfileImageFile(null)} className="w-4 absolute top-5 right-0 z-50 cursor-pointer"/>
           </div>
 
+          <div className="w-2/3">
+            {/* 닉네임 */}
+            <EditInput title="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+            {nicknameError && <p className="text-sm text-red-500 ml-1">{nicknameError}</p>}
 
-          {/* 닉네임 */}
-          <EditInput title="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-
-          {/* 상태메세지 */}
-          <EditInput title="상태메세지" value={message} onChange={(e) => setMessage(e.target.value)} />        
-
+            {/* 상태메세지 */}
+            <EditInput title="상태메세지" value={message} onChange={(e) => setMessage(e.target.value)} />        
+            {messageError && <p className="text-sm text-red-500 ml-2">{messageError}</p>}
+          </div>
 
         {/* 버튼 */}
         <div className="flex w-full pl-6 pr-6 justify-between mt-15 absolute bottom-5 bg-white">
