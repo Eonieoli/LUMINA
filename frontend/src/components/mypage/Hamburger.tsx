bfbb0622c19a97c532afa3e81ca57a1f5a0dd695
김민정 <99minj0731@naver.com>
2025-05-14 10:16:13 +0900

    style: 햄버거 버튼 페이지 디자인 수정

--- frontend/src/components/mypage/Hamburger.tsx
+++ frontend/src/components/mypage/Hamburger.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { signOut } from "@/apis/auth";
 import { Rewards, Subscribes } from './HamburgerChildren'
 import { useLocation } from "react-router-dom";
+import { XIcon } from "@/assets/images";
 
 export interface HamburgerProps {
     isVisible: boolean;
@@ -47,27 +48,27 @@ export default function Hamburger({isVisible, onClose}: HamburgerProps) {
                     animate={{ opacity: 1, x: 0 }}  // 등장 시
                     exit={{ opacity: 0, x: 100 }}    // 사라질 때
                     transition={{ duration: 0.4 }}
-                    className="fixed md:absolute z-50 w-full h-full bg-[#eeeeee]"
+                    className="fixed md:absolute z-50 w-full h-full bg-[#ffffff]"
                 >
                     <div className="relative w-full h-full flex flex-col">
-                        <div className="relative flex justify-center items-center text-2xl font-bold p-4">
+                        <div className="relative flex justify-center items-center text-xl font-semibold p-6 text-gray-700">
                             설정
-                            <div className="absolute left-4 cursor-pointer" onClick={onClose}>✕</div>
+                            <img  src={XIcon}className=" w-4 absolute left-6 cursor-pointer" onClick={onClose}/>
                         </div>
-                        <div className="flex-1 flex flex-col justify-between text-xl font-semibold my-4">
-                            <div className="flex flex-col gap-y-2">
-                                <div className="p-4 px-10 cursor-pointer">내 기부 내역</div>
+                        <div className="flex-1 flex flex-col justify-between text-[18px] font-medium">
+                            <div className="flex flex-col gap-2 text-gray-600">
+                                <div className="p-2 px-10 cursor-pointer">내 기부 내역</div>
                                 <div onClick={() => {
                                     setIsRewardsOpened(true)
                                     setIsRewardsAnimation(true)}}
-                                    className="p-4 px-10 cursor-pointer"
+                                    className="p-2 px-10 cursor-pointer"
                                 >
                                     리워드 내역
                                     </div>
                                 <div onClick={() => {
                                     setIsSubscribeOpened(true)
                                     setIsSubscribeAnimation(true)}}
-                                    className="p-4 px-10 cursor-pointer"
+                                    className="p-2 px-10 cursor-pointer"
                                 >
                                     구독 카테고리
                                 </div>
