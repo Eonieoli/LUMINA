b51b0073931cc2a3fff7827b8e234299bb2b14cb
김민정 <99minj0731@naver.com>
2025-05-14 10:18:36 +0900

    style: 관심 카테고리 테두리 색상 수정

--- frontend/src/components/mypage/HamburgerChildren/Subscribes.tsx
+++ frontend/src/components/mypage/HamburgerChildren/Subscribes.tsx
@@ -53,7 +53,7 @@ export default function Subscribes({isVisible, onClose}: HamburgerProps) {
 
                     {/* 카테고리 목록 */}
                     <div className="flex-1 flex flex-col justify-between text-xl font-medium text-gray-700 ">
-                        <div className="flex flex-col gap-y-1 px-10 py-5 border-2 mx-6 rounded-2xl border-gray-400">
+                        <div className="flex flex-col gap-y-1 px-10 py-5 border-2 mx-6 rounded-2xl border-gray-200">
                             {categories.map((category) => (
                                 <div key={category.categoryId} className="flex justify-between py-2">
                                     <div>{category.categoryName}</div>
