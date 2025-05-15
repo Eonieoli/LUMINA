c034248bbaaab872374b6ced457760da52289c3b
김민정 <99minj0731@naver.com>
2025-05-15 09:34:37 +0900

    feat: 기부처 사진 업로드

--- frontend/src/components/donate/DonationImageMap.ts
+++ frontend/src/components/donate/DonationImageMap.ts
@@ -1,7 +1,109 @@
 import defaultDonationThumbnail from '@/assets/images/donate/donationThumbNail.jpg';
+import img52 from '@/assets/images/donationsthumbnail/52.png'
+import img53 from '@/assets/images/donationsthumbnail/53.png';
+import img54 from '@/assets/images/donationsthumbnail/54.png';
+import img55 from '@/assets/images/donationsthumbnail/55.png';
+import img56 from '@/assets/images/donationsthumbnail/56.png';
+import img57 from '@/assets/images/donationsthumbnail/57.png';
+import img58 from '@/assets/images/donationsthumbnail/58.png';
+import img59 from '@/assets/images/donationsthumbnail/59.png';
+import img60 from '@/assets/images/donationsthumbnail/60.png';
+import img61 from '@/assets/images/donationsthumbnail/61.png';
+import img62 from '@/assets/images/donationsthumbnail/62.png';
+import img63 from '@/assets/images/donationsthumbnail/63.png';
+import img64 from '@/assets/images/donationsthumbnail/64.png';
+import img65 from '@/assets/images/donationsthumbnail/65.png';
+import img66 from '@/assets/images/donationsthumbnail/66.png';
+import img67 from '@/assets/images/donationsthumbnail/67.png';
+import img68 from '@/assets/images/donationsthumbnail/68.png';
+import img69 from '@/assets/images/donationsthumbnail/69.png';
+import img70 from '@/assets/images/donationsthumbnail/70.png';
+import img71 from '@/assets/images/donationsthumbnail/71.png';
+import img72 from '@/assets/images/donationsthumbnail/72.png';
+import img73 from '@/assets/images/donationsthumbnail/73.png';
+import img74 from '@/assets/images/donationsthumbnail/74.png';
+import img75 from '@/assets/images/donationsthumbnail/75.png';
+import img76 from '@/assets/images/donationsthumbnail/76.png';
+import img77 from '@/assets/images/donationsthumbnail/77.png';
+import img78 from '@/assets/images/donationsthumbnail/78.png';
+import img79 from '@/assets/images/donationsthumbnail/79.png';
+import img80 from '@/assets/images/donationsthumbnail/80.png';
+import img81 from '@/assets/images/donationsthumbnail/81.png';
+import img82 from '@/assets/images/donationsthumbnail/82.png';
+import img83 from '@/assets/images/donationsthumbnail/83.png';
+import img84 from '@/assets/images/donationsthumbnail/84.png';
+import img85 from '@/assets/images/donationsthumbnail/85.png';
+import img86 from '@/assets/images/donationsthumbnail/86.png';
+import img87 from '@/assets/images/donationsthumbnail/87.png';
+import img88 from '@/assets/images/donationsthumbnail/88.png';
+import img89 from '@/assets/images/donationsthumbnail/89.png';
+import img90 from '@/assets/images/donationsthumbnail/90.png';
+import img91 from '@/assets/images/donationsthumbnail/91.png';
+import img92 from '@/assets/images/donationsthumbnail/92.png';
+import img93 from '@/assets/images/donationsthumbnail/93.png';
+import img94 from '@/assets/images/donationsthumbnail/94.png';
+import img95 from '@/assets/images/donationsthumbnail/95.png';
+import img96 from '@/assets/images/donationsthumbnail/96.png';
+import img97 from '@/assets/images/donationsthumbnail/97.png';
+import img98 from '@/assets/images/donationsthumbnail/98.png';
+import img99 from '@/assets/images/donationsthumbnail/99.png';
+import img100 from '@/assets/images/donationsthumbnail/100.png';
+import img101 from '@/assets/images/donationsthumbnail/101.png';
+
+
 
 const donationImageMap: Record<number, string> = {
     // donationId랑 이미지랑 매핑해서 넣을 곳
+    52: img52,
+    53: img53,
+    54: img54,
+    55: img55,
+    56: img56,
+    57: img57,
+    58: img58,
+    59: img59,
+    60: img60,
+    61: img61,
+    62: img62,
+    63: img63,
+    64: img64,
+    65: img65,
+    66: img66,
+    67: img67,
+    68: img68,
+    69: img69,
+    70: img70,
+    71: img71,
+    72: img72,
+    73: img73,
+    74: img74,
+    75: img75,
+    76: img76,
+    77: img77,
+    78: img78,
+    79: img79,
+    80: img80,
+    81: img81,
+    82: img82,
+    83: img83,
+    84: img84,
+    85: img85,
+    86: img86,
+    87: img87,
+    88: img88,
+    89: img89,
+    90: img90,
+    91: img91,
+    92: img92,
+    93: img93,
+    94: img94,
+    95: img95,
+    96: img96,
+    97: img97,
+    98: img98,
+    99: img99,
+    100: img100,
+    101: img101,
 };
 
 // 일단 임시 사진으로 저장 내보내기
