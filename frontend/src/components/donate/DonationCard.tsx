a62e55f5110628168dd35a330685baa374b7fc52
김민정 <99minj0731@naver.com>
2025-05-15 14:34:33 +0900

    fix: donationCard useParams 삭제

--- frontend/src/components/donate/DonationCard.tsx
+++ frontend/src/components/donate/DonationCard.tsx
@@ -1,5 +1,5 @@
 import { donationImageMap, defaultDonationThumbnail } from "./DonationImageMap"
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate} from "react-router-dom"
 
 export interface DonationProps {
     donationId: number;
