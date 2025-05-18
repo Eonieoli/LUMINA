9564f7a5b0085ee3e6822c996e69ea359caca322
홍석진 <seokbangguri@kakao.com>
2025-05-18 23:58:50 +0900

    feat: xcode ios 앱 개발
    S12P31S306-327

---
+++ IOS/LUMINA/LUMINATests/LUMINATests.swift
@@ -0,0 +1,17 @@
+//
+//  LUMINATests.swift
+//  LUMINATests
+//
+//  Created by 홍석진 on 5/18/25.
+//
+
+import Testing
+@testable import LUMINA
+
+struct LUMINATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
